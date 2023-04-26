540e1cf462d0d37f5ca0b92d6a9daee7013a253a
Tobias Marzell <tobias.marzell@gmail.com>
2023-04-26 18:42:27 +0200

    changes glad library from multiple context to single context

--- dependencies/setup_glad.cmake
+++ dependencies/setup_glad.cmake
@@ -3,16 +3,20 @@ Include(FetchContent)
 FetchContent_Declare(
         glad
         GIT_REPOSITORY https://github.com/Dav1dde/glad
-        GIT_TAG        d08b1aa  # v2.0.4
+        GIT_TAG d08b1aa  # v2.0.4
         FIND_PACKAGE_ARGS
 )
 
 FetchContent_GetProperties(glad)
-set(GLAD_PROFILE "core"	CACHE STRING "OpenGL profile")
+set(GLAD_PROFILE "core" CACHE STRING "OpenGL profile")
 
-set(GLAD_GENERATOR "c"	CACHE STRING "Language to generate the binding for")
+set(GLAD_GENERATOR "c" CACHE STRING "Language to generate the binding for")
 
 FetchContent_MakeAvailable(glad)
 
 add_subdirectory(${glad_SOURCE_DIR}/cmake glad_cmake)
-glad_add_library(glad_gl_core_mx_33 REPRODUCIBLE MX API gl:core=3.3)  # TODO: check what this does exactly
+
+# add the library with given options
+# check https://github.com/Dav1dde/glad/blob/glad2/cmake/CMakeLists.txt for more information
+glad_add_library(glad_gl_core_33 REPRODUCIBLE API gl:core=3.3)
+
