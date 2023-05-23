1e12241b694aac84125622ca0a0850d536cb1ab6
Tobias Marzell <tobias.marzell@gmail.com>
2023-05-23 23:15:23 +0200

    small refactoring for recent changes

--- dependencies/setup_imgui.cmake
+++ dependencies/setup_imgui.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
         ImGui
         GIT_REPOSITORY https://github.com/ocornut/imgui
-        GIT_TAG        1ebb913  # v1.89.5
+        GIT_TAG 1ebb913  # v1.89.5
 )
 
 FetchContent_MakeAvailable(ImGui)
@@ -11,14 +11,17 @@ FetchContent_MakeAvailable(ImGui)
 
 # example setup see: https://github.com/ocornut/imgui/blob/master/examples/example_glfw_opengl3/Makefile
 add_library(imgui_glfw
-    ${imgui_SOURCE_DIR}/imgui.cpp
-    ${imgui_SOURCE_DIR}/imgui_draw.cpp
-    ${imgui_SOURCE_DIR}/imgui_demo.cpp
-    ${imgui_SOURCE_DIR}/imgui_tables.cpp
-    ${imgui_SOURCE_DIR}/imgui_widgets.cpp
+        ${imgui_SOURCE_DIR}/imgui.cpp
+        ${imgui_SOURCE_DIR}/imgui_draw.cpp
+        ${imgui_SOURCE_DIR}/imgui_demo.cpp
+        ${imgui_SOURCE_DIR}/imgui_tables.cpp
+        ${imgui_SOURCE_DIR}/imgui_widgets.cpp
 
-    ${imgui_SOURCE_DIR}/backends/imgui_impl_glfw.cpp
-    ${imgui_SOURCE_DIR}/backends/imgui_impl_opengl3.cpp
-)
+        ${imgui_SOURCE_DIR}/backends/imgui_impl_glfw.cpp
+        ${imgui_SOURCE_DIR}/backends/imgui_impl_opengl3.cpp
+        )
 
-target_include_directories(imgui_glfw PUBLIC ${imgui_SOURCE_DIR} ${imgui_SOURCE_DIR}/backends)
+target_include_directories(imgui_glfw
+        PUBLIC
+        ${imgui_SOURCE_DIR}
+        ${imgui_SOURCE_DIR}/backends)
