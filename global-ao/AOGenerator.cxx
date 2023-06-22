ffdf30ca4866be03d653191bfee071f80005c7fd
karlfredo <90260517+karlfreduo@users.noreply.github.com>
2023-06-22 11:30:08 +0200

    added bvh structure

---
+++ global-ao/AOGenerator.cxx
@@ -0,0 +1,153 @@
+#include "AOGenerator.hxx"
+
+
+bool rayTriangleTest(glm::vec3 origin, glm::vec3 direction, glm::uint index, std::vector<Vertex>* vertices, std::vector<Triangle>* triangles){
+    // https://en.wikipedia.org/wiki/M%C3%B6ller%E2%80%93Trumbore_intersection_algorithm
+    const float EPSILON = 0.0000001;
+    auto tri = (*triangles)[index];
+    glm::vec3 v0 = (*vertices)[tri.x].position;
+    glm::vec3 v1 = (*vertices)[tri.y].position;
+    glm::vec3 v2 = (*vertices)[tri.z].position;
+    glm::vec3 edge1, edge2, h, s, q;
+    float a, f, u, v;
+    edge1 = v1-v0;
+    edge2 = v2-v0;
+    h = glm::cross(direction, edge2);
+    a = glm::dot(edge1, h);
+
+    if (a > -EPSILON && a < EPSILON)
+        return false;    // This ray is parallel to this triangle.
+
+    f = 1.0 / a;
+    s = origin - v0;
+    u = f * glm::dot(s, h);
+
+    if (u < 0.0 || u > 1.0)
+        return false;
+
+    q = glm::cross(s, edge1);
+    v = f * dot(direction, q);
+
+    if (v < 0.0 || u + v > 1.0)
+        return false;
+
+    // At this stage we can compute t to find out where the intersection point is on the line.
+    float t = f * glm::dot(edge2, q);
+
+    if (t > EPSILON) // ray intersection
+    {
+        return true;
+    }
+    else // This means that there is a line intersection but not a ray intersection.
+        return false;
+}
+
+/*
+bool rayAABBTest(AABB& aabb, glm::vec3 origin, glm::vec3 dir)
+{
+    // reference https://web.archive.org/web/20090803054252/http://tog.acm.org/resources/GraphicsGems/gems/RayBox.c
+    bool inside = true;
+	char quadrant[3];
+	int whichPlane;
+	double maxT[3];
+	double candidatePlane[3];
+
+    const int NUMDIM = 3;
+    const int RIGHT = 0;
+    const int LEFT = 1;
+    const int MIDDLE = 2;
+
+    const bool FALSE = false;
+    const bool TRUE = false;
+
+    glm::vec3& minB = aabb.min;
+    glm::vec3& maxB = aabb.max;
+
+    glm::vec3 coord;
+
+	// Find candidate planes; this loop can be avoided if rays cast all from the eye(assume perpsective view)
+	for (int i=0; i<3; i++){
+		if(origin[i] < minB[i]) {
+			quadrant[i] = LEFT;
+			candidatePlane[i] = minB[i];
+			inside = FALSE;
+		}else if (origin[i] > maxB[i]) {
+			quadrant[i] = RIGHT;
+			candidatePlane[i] = maxB[i];
+			inside = FALSE;
+		}else	{
+			quadrant[i] = MIDDLE;
+        }
+    }
+		
+
+	// Ray origin inside bounding box
+	if(inside)	{
+		coord = origin;
+		return (TRUE);
+	}
+
+
+	// Calculate T distances to candidate planes
+	for (int i = 0; i < NUMDIM; i++)
+		if (quadrant[i] != MIDDLE && dir[i] !=0.)
+			maxT[i] = (candidatePlane[i]-origin[i]) / dir[i];
+		else
+			maxT[i] = -1.;
+
+	// Get largest of the maxT's for final choice of intersection
+	whichPlane = 0;
+	for (int i = 1; i < NUMDIM; i++)
+		if (maxT[whichPlane] < maxT[i])
+			whichPlane = i;
+
+	// Check final candidate actually inside box
+	if (maxT[whichPlane] < 0.) return (FALSE);
+	for (int i = 0; i < NUMDIM; i++)
+		if (whichPlane != i) {
+			coord[i] = origin[i] + maxT[whichPlane] *dir[i];
+			if (coord[i] < minB[i] || coord[i] > maxB[i])
+				return (FALSE);
+		} else {
+			coord[i] = candidatePlane[i];
+		}
+	return (TRUE);// ray hits box
+}
+//*/
+
+glm::vec3 spherePoint(){
+    for (int i = 0; i < 10; ++i){
+        float x = 2.0*float(rand())/float(RAND_MAX)-1.0;
+        float y = 2.0*float(rand())/float(RAND_MAX)-1.0;
+        float z = 2.0*float(rand())/float(RAND_MAX)-1.0;
+        glm::vec3 dir = glm::vec3(x, y, z);
+        if (glm::length(dir) < 1.0)
+        {
+            glm::normalize(dir);
+            return dir;
+        }
+    }
+    return glm::vec3(0, 0, 0);
+}
+
+bool layerOutput(std::vector<Vertex>* vertices, std::vector<Triangle>* triangles) {
+    for(int i = 0; i < vertices->size(); ++i){
+        auto& vtx = (*vertices)[i];
+        int samples = 1;
+        int sum = 0;
+        for (int d = 0; d < samples; ++d){
+            bool hitFound = false;
+            auto hemidir = spherePoint();
+            hemidir = glm::dot(hemidir, vtx.normal)<0?-hemidir:hemidir;
+            for (int t = 0; t < triangles->size(); ++t){
+                auto hit = rayTriangleTest(vtx.position, hemidir, t, vertices, triangles);
+                hitFound |= hit;
+                if (hit) break;
+            }
+            if (hitFound) ++sum;
+        }
+        float value = float(sum)/float(samples);
+        vtx.color = glm::vec4(1.0f-value, 1.0f-value, 1.0f-value, 1.0);
+    }
+    return true;
+}
--- global-ao/AOGenerator.hxx
+++ global-ao/AOGenerator.hxx
@@ -1,81 +1,8 @@
 #pragma once
 #include "TriangleMesh.hxx"
 
-bool rayTriangleTest(glm::vec3 origin, glm::vec3 direction, glm::uint index, std::vector<Vertex>* vertices, std::vector<Triangle>* triangles){
-    // https://en.wikipedia.org/wiki/M%C3%B6ller%E2%80%93Trumbore_intersection_algorithm
-    const float EPSILON = 0.0000001;
-    auto tri = (*triangles)[index];
-    glm::vec3 v0 = (*vertices)[tri.x].position;
-    glm::vec3 v1 = (*vertices)[tri.y].position;
-    glm::vec3 v2 = (*vertices)[tri.z].position;
-    glm::vec3 edge1, edge2, h, s, q;
-    float a, f, u, v;
-    edge1 = v1-v0;
-    edge2 = v2-v0;
-    h = glm::cross(direction, edge2);
-    a = glm::dot(edge1, h);
+bool rayTriangleTest(glm::vec3 origin, glm::vec3 direction, glm::uint index, std::vector<Vertex>* vertices, std::vector<Triangle>* triangles);
 
-    if (a > -EPSILON && a < EPSILON)
-        return false;    // This ray is parallel to this triangle.
+glm::vec3 spherePoint();
 
-    f = 1.0 / a;
-    s = origin - v0;
-    u = f * glm::dot(s, h);
-
-    if (u < 0.0 || u > 1.0)
-        return false;
-
-    q = glm::cross(s, edge1);
-    v = f * dot(direction, q);
-
-    if (v < 0.0 || u + v > 1.0)
-        return false;
-
-    // At this stage we can compute t to find out where the intersection point is on the line.
-    float t = f * glm::dot(edge2, q);
-
-    if (t > EPSILON) // ray intersection
-    {
-        return true;
-    }
-    else // This means that there is a line intersection but not a ray intersection.
-        return false;
-}
-
-
-glm::vec3 spherePoint(){
-    for (int i = 0; i < 10; ++i){
-        float x = 2.0*float(rand())/float(RAND_MAX)-1.0;
-        float y = 2.0*float(rand())/float(RAND_MAX)-1.0;
-        float z = 2.0*float(rand())/float(RAND_MAX)-1.0;
-        glm::vec3 dir = glm::vec3(x, y, z);
-        if (glm::length(dir) < 1.0)
-        {
-            glm::normalize(dir);
-            return dir;
-        }
-    }
-    return glm::vec3(0, 0, 0);
-}
-
-
-bool layerOutput(std::vector<Vertex>* vertices, std::vector<Triangle>* triangles) {
-    for(int i = 0; i < vertices->size(); ++i){
-        auto& vtx = (*vertices)[i];
-        int samples = 2;
-        int sum = 0;
-        for (int d = 0; d < samples; ++d){
-            bool hitFound = false;
-            auto dir = spherePoint();
-            dir = glm::dot(dir, vtx.normal)<0?-dir:dir;
-            for (int t = 0; t < triangles->size(); ++t){
-                auto hit = rayTriangleTest(vtx.position, dir, t, vertices, triangles);
-                hitFound |= hit;
-            }
-            if (hitFound) ++sum;
-        }
-        float value = float(sum)/float(samples);
-        vtx.color = glm::vec4(1.0f-value, 1.0f-value, 1.0f-value, 1.0);
-    }
-    return true;
-}
+bool layerOutput(std::vector<Vertex>* vertices, std::vector<Triangle>* triangles);
